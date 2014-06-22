1ac6cdae70953d3e5d3e3c01a09b4de98888fbd0
blackazaru <user@samsung>
2014-06-22 14:53:31 +0400

    Add image to Menu

--- android/src/com/FortPinnsvin/fallingman/android/AndroidLauncher.java
+++ android/src/com/FortPinnsvin/fallingman/android/AndroidLauncher.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.FortPinnsvin.fallingman.AndroidRun;
+import com.FortPinnsvin.fallingman.Menu;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
--- core/src/com/FortPinnsvin/fallingman/AndroidRun.java
+++ core/src/com/FortPinnsvin/fallingman/AndroidRun.java
@@ -1,27 +1,66 @@
 package com.FortPinnsvin.fallingman;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class AndroidRun extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class AndroidRun implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Sprite sprite;
+	private Texture menu;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(1, w / h);
 		batch = new SpriteBatch();
-		img = new Texture("menu.png");
+		menu = new Texture("menuu.png");
+		sprite = new Sprite(menu);
+		sprite.setSize(1.0f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+		sprite.rotate90(false);
+		sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);	
+		
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
 	}
 }
