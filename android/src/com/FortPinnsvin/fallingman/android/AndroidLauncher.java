c82c25faaf3683e370d632bf4a89fe0777f879e2
blackazaru <user@samsung>
2014-06-22 16:54:06 +0400

    Good orientation and new class Menu

--- android/src/com/FortPinnsvin/fallingman/android/AndroidLauncher.java
+++ android/src/com/FortPinnsvin/fallingman/android/AndroidLauncher.java
@@ -1,9 +1,12 @@
 package com.FortPinnsvin.fallingman.android;
 
 import android.os.Bundle;
+import android.widget.Gallery;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 import com.FortPinnsvin.fallingman.AndroidRun;
 import com.FortPinnsvin.fallingman.Menu;
 
--- core/src/com/FortPinnsvin/fallingman/AndroidRun.java
+++ core/src/com/FortPinnsvin/fallingman/AndroidRun.java
@@ -10,34 +10,17 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AndroidRun implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Sprite sprite;
-	private Texture menu;
+	Menu menu;
 	
 	@Override
 	public void create () {
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		camera = new OrthographicCamera(1, w / h);
-		batch = new SpriteBatch();
-		menu = new Texture("menuu.png");
-		sprite = new Sprite(menu);
-		sprite.setSize(1.0f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
-		sprite.rotate90(false);
-		sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);	
-		
+		menu = new Menu();
+		menu.create();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		menu.render();
 	}
 
 	@Override
